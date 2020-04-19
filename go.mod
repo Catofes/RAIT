0cfeadb0a6f943c424a5f85eb2afc7ca23d32295
Nick Cao <nickcao@nichi.co>
2020-04-19 17:04:58 +0800

    creates netns if not exist

--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/vishvananda/netns v0.0.0-20191106174202-0a2b9b5464df
 	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71 // indirect
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
-	golang.org/x/sys v0.0.0-20200409092240-59c9f1ba88fa // indirect
+	golang.org/x/sys v0.0.0-20200409092240-59c9f1ba88fa
 	golang.zx2c4.com/wireguard v0.0.20200320 // indirect
 	golang.zx2c4.com/wireguard/wgctrl v0.0.0-20200324154536-ceff61240acf
 	gopkg.in/yaml.v2 v2.2.8 // indirect
--- rait/internal/utils/netlink.go
+++ rait/internal/utils/netlink.go
@@ -4,6 +4,11 @@ import (
 	"fmt"
 	"github.com/vishvananda/netlink"
 	"github.com/vishvananda/netns"
+	"golang.org/x/sys/unix"
+	"os"
+	"path"
+	"runtime"
+	"sync"
 )
 
 // NetlinkHelper handles the lifecycle of NetlinkHandle and NamespaceHandle
@@ -37,6 +42,10 @@ func NetlinkHelperFromName(name string) (*NetlinkHelper, error) {
 			return nil, fmt.Errorf("failed to get netlink handle: %w", err)
 		}
 	default:
+		err = CreateNamespaceFromNameIfNotExist(name)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create named namespace: %w", err)
+		}
 		helper.NamespaceHandle, err = netns.GetFromName(name)
 		if err != nil {
 			return nil, fmt.Errorf("failed to get namespace handle: %w", err)
@@ -47,4 +56,64 @@ func NetlinkHelperFromName(name string) (*NetlinkHelper, error) {
 		}
 	}
 	return &helper, nil
+}
+
+// CreateNamespaceFromNameIfNotExist creates named netns in the runtime dir
+// It serves as a replacement of ip netns add
+func CreateNamespaceFromNameIfNotExist(name string) error {
+	var runtimeDir = "/run/netns"
+	var nsPath = path.Join(runtimeDir, name)
+	var handle netns.NsHandle
+	var err error
+	handle, err = netns.GetFromName(name)
+	if err == nil {
+		handle.Close()
+		return nil
+	}
+	// Don't touch it if the runtime dir exists
+	_, err = os.Stat(runtimeDir)
+	if err != nil {
+		err = os.MkdirAll(runtimeDir, 0755)
+		if err != nil {
+			return fmt.Errorf("failed to create runtime dir: %w", err)
+		}
+		err = unix.Mount("tmpfs", runtimeDir, "tmpfs", unix.MS_NOSUID|unix.MS_NODEV, "mode=755")
+		if err != nil {
+			return fmt.Errorf("failed to mount tmpfs onto runtime dir: %w", err)
+		}
+	}
+
+	var nsFd *os.File
+	nsFd, err = os.Create(nsPath)
+	if err != nil {
+		return fmt.Errorf("failed to create fd for ns: %w", err)
+	}
+	err = nsFd.Close()
+	if err != nil {
+		return fmt.Errorf("failed to close fd for ns: %w", err)
+	}
+	defer os.RemoveAll(nsPath)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go (func() {
+		defer wg.Done()
+		runtime.LockOSThread()
+		threadNsPath := fmt.Sprintf("/proc/%d/task/%d/ns/net", os.Getpid(), unix.Gettid())
+		err = unix.Unshare(unix.CLONE_NEWNET)
+		if err != nil {
+			err = fmt.Errorf("failed to unshare netns: %w", err)
+			return
+		}
+		err = unix.Mount(threadNsPath, nsPath, "none", unix.MS_BIND|unix.MS_SHARED|unix.MS_REC, "")
+		if err != nil {
+			err = fmt.Errorf("failed to bind mount ns at %s: %w", nsPath, err)
+			return
+		}
+	})()
+	wg.Wait()
+	if err != nil {
+		return fmt.Errorf("failed to create namespace: %w", err)
+	}
+	return nil
 }
