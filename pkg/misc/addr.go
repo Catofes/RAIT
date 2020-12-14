297c2eb63cdf17999c505580087784c8774b91ff
Catofes <i@catofes.com>
2020-12-14 21:36:35 +0800

    Drop NewLLAddr(), Use NewLLAddrFromMac() Only

--- pkg/misc/addr.go
+++ pkg/misc/addr.go
@@ -4,9 +4,7 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
-	"math/rand"
 	"net"
-	"time"
 
 	"github.com/Catofes/netlink"
 	"go.uber.org/zap"
@@ -21,28 +19,28 @@ var _16b = [16]byte{}
 var IPNetAll = []net.IPNet{{IP: _4b[:], Mask: _4b[:]}, {IP: _16b[:], Mask: _16b[:]}}
 
 // NewLLAddr generates a RFC 4862 compliant linklocal address, from a random mac address
-func NewLLAddr() *netlink.Addr {
-	rand.Seed(time.Now().UnixNano())
-	digits := []int{0x00, 0x16, 0x3e, rand.Intn(0x7f + 1), rand.Intn(0xff + 1), rand.Intn(0xff + 1)}
-	digits = append(digits, 0, 0)
-	copy(digits[5:], digits[3:])
-	digits[3] = 0xff
-	digits[4] = 0xfe
-	digits[0] = digits[0] ^ 2
-	var parts string
-	for i := 0; i < len(digits); i += 2 {
-		parts += ":"
-		parts += fmt.Sprintf("%x", digits[i])
-		parts += fmt.Sprintf("%x", digits[i+1])
-	}
-	addr, _ := netlink.ParseAddr("fe80:" + parts + "/64")
-	return addr
-}
+// func NewLLAddr() *netlink.Addr {
+// 	rand.Seed(time.Now().UnixNano())
+// 	digits := []int{0x00, 0x16, 0x3e, rand.Intn(0x7f + 1), rand.Intn(0xff + 1), rand.Intn(0xff + 1)}
+// 	digits = append(digits, 0, 0)
+// 	copy(digits[5:], digits[3:])
+// 	digits[3] = 0xff
+// 	digits[4] = 0xfe
+// 	digits[0] = digits[0] ^ 2
+// 	var parts string
+// 	for i := 0; i < len(digits); i += 2 {
+// 		parts += ":"
+// 		parts += fmt.Sprintf("%x", digits[i])
+// 		parts += fmt.Sprintf("%x", digits[i+1])
+// 	}
+// 	addr, _ := netlink.ParseAddr("fe80:" + parts + "/64")
+// 	return addr
+// }
 
 func NewLLAddrFromMac(mac net.HardwareAddr) *netlink.Addr {
-	if len(mac) != 6 {
-		return NewLLAddr()
-	}
+	// if len(mac) != 6 {
+	// 	return NewLLAddr()
+	// }
 	digits := make([]byte, 8)
 	copy(digits[:], mac[:])
 	copy(digits[5:], digits[3:])
